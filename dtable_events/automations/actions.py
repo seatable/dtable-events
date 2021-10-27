00e0e60c72e6d45c27a747f9af0f16388e119fee
r350178982 <32759763+r350178982@users.noreply.github.com>
2021-10-27 13:05:25 +0800

    Update actions.py

--- dtable_events/automations/actions.py
+++ dtable_events/automations/actions.py
@@ -1236,14 +1236,15 @@ class AutomationRule:
     def set_task_log(self):
         try:
             set_task_log_sql = """
-                INSERT INTO auto_rules_task_log (trigger_time, success, rule_id) VALUES
-                (:trigger_time, :success, :rule_id)
+                INSERT INTO auto_rules_task_log (trigger_time, success, rule_id, run_condition) VALUES
+                (:trigger_time, :success, :rule_id, :run_condition)
             """
             if self.run_condition in (PER_DAY, PER_WEEK, PER_MONTH):
                 self.db_session.execute(set_task_log_sql, {
                     'trigger_time': datetime.utcnow(),
                     'success': self.task_run_seccess,
-                    'rule_id': self.rule_id
+                    'rule_id': self.rule_id,
+                    'run_condition': self.run_condition,
                 })
                 self.db_session.commit()
         except Exception as e:
