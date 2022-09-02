e109a2a8d28abe0a811132753eea373ca584df6f
AlexCXC <1223408988@qq.com>
2022-09-02 18:26:15 +0800

    near deadline rows from dtable-db and add message formatters for rows from dtable-db
    init message formatters
    
    opt formatters
    
    debug formatter
    
    opt formatters
    
    opt sql rows formatters
    
    opt code
    
    remove useless code
    
    remove useless code

--- dtable_events/notification_rules/notification_rules_utils.py
+++ dtable_events/notification_rules/notification_rules_utils.py
@@ -1,18 +1,24 @@
 # -*- coding: utf-8 -*-
-import logging
-import time
 import json
-from datetime import datetime, date, timedelta
-import requests
-import jwt
+import logging
 import re
+import time
+from datetime import datetime, date, timedelta
 
-from dtable_events.app.config import DTABLE_PRIVATE_KEY, DTABLE_WEB_SERVICE_URL
-from dtable_events.cache import redis_cache as cache
+import jwt
+import requests
+from dateutil import parser
+
+from dtable_events import filter2sql
+from dtable_events.app.config import DTABLE_PRIVATE_KEY, DTABLE_WEB_SERVICE_URL, INNER_DTABLE_DB_URL
+from dtable_events.notification_rules.utils import get_nickname_by_usernames
 from dtable_events.utils import is_valid_email, uuid_str_to_36_chars, get_inner_dtable_server_url
 from dtable_events.utils.constants import ColumnTypes
 from dtable_events.utils.dtable_server_api import DTableServerAPI
 from dtable_events.utils.dtable_web_api import DTableWebAPI
+from dtable_events.utils.dtable_db_api import DTableDBAPI
+from dtable_events.utils.sql_generator import BaseSQLGenerator
+from dtable_events.notification_rules.message_formatters import create_formatter_params, formatter_map
 
 logger = logging.getLogger(__name__)
 
@@ -100,70 +106,75 @@ def deal_invalid_rule(rule_id, db_session):
         logger.error(e)
 
 
-def list_rows_near_deadline(dtable_uuid, table_id, view_id, date_column_name, alarm_days, dtable_server_access_token, rule_id=None, db_session=None):
-    api_url = get_inner_dtable_server_url()
-    url = api_url.rstrip('/') + '/api/v1/internal/dtables/' + uuid_str_to_36_chars(dtable_uuid) + '/list-rows-near-deadline/?from=dtable_events'
-    headers = {'Authorization': 'Token ' + dtable_server_access_token}
-    now_date = date.today()
-    now_plus_alarm_date = now_date + timedelta(days=int(alarm_days))
-    query_params = {
-        'table_id': table_id,
-        'view_id': view_id,
-        'date_column_name': date_column_name,
-        'now_date': str(now_date),
-        'end_date': str(now_plus_alarm_date),
+class NearDeadlineSqlGenerator(BaseSQLGenerator):
+
+    def to_sql(self, date_column_name, alarm_days, by_group=False):
+        sql = "%s `%s`" % (
+            "SELECT * FROM",
+            self.table_name
+        )
+        if not by_group:
+            filter_clause = self._filter2sql()
+            sort_clause = self._sort2sql()
+            limit_clause = self._limit2sql()
+        else:
+            filter_clause = self._groupfilter2sql()
+            sort_clause = self._sort2sql(by_group=True)
+            limit_clause = self._limit2sql(by_group=True)
+
+        now_date = date.today()
+        now_plus_alarm_date = now_date + timedelta(days=int(alarm_days))
+        days_filter_snippet = "`%(date_column_name)s`>='%(now_date)s' AND `%(date_column_name)s`<='%(end_date)s'" % {
+            'date_column_name': date_column_name,
+            'now_date': str(now_date) + ' 00:00',
+            'end_date': str(now_plus_alarm_date) + ' 23:59'
+        }
+
+        if filter_clause:
+            filter_clause = 'WHERE (%s) AND (%s)' % (
+                filter_clause[len('WHERE '):],
+                days_filter_snippet
+            )
+            sql = "%s %s" % (sql, filter_clause)
+        else:
+            sql = "%s %s" % (sql, 'WHERE ' + days_filter_snippet)
+        if sort_clause:
+            sql = "%s %s" % (sql, sort_clause)
+        if limit_clause:
+            sql = "%s %s" % (sql, limit_clause)
+        return sql
+
+
+def list_rows_near_deadline_with_dtable_db(dtable_metadata, table_id, view_id, date_column_name, alarm_days, dtable_db_api):
+    """
+    return: rows -> list or None, metate -> dict or None
+    """
+    table, view = None, None
+    for tmp_table in dtable_metadata['tables']:
+        if tmp_table['_id'] == table_id:
+            table = tmp_table
+            for tmp_view in table.get('views', []):
+                if tmp_view['_id'] == view_id:
+                    view = tmp_view
+                    break
+            break
+    if not table or not view:
+        return [], None
+    filters = view.get('filters', [])
+    filter_conjunction = view.get('filter_conjunction', 'And')
+    filter_conditions = {
+        'start': 0,
         'limit': 25
     }
-    try:
-        res = requests.get(url, headers=headers, params=query_params)
-    except Exception as e:
-        logger.error('dtable: %s request list-rows-near-deadline error: %s', dtable_uuid, e)
-        return []
-
-    if res.status_code == 404:
-        deal_invalid_rule(rule_id, db_session)
-        logger.warning('dtable: %s request list-rows-near-deadline error status code: %s, response text: %s', dtable_uuid, res.status_code, res.text)
-        return []
-    if res.status_code != 200:
-        logger.error('dtable: %s request list-rows-near-deadline error status code: %s, response text: %s', dtable_uuid, res.status_code, res.text)
-        return []
-
-    rows = json.loads(res.content).get('rows', [])
-    return rows
-
-
-def get_nickname_by_usernames(usernames, db_session):
-    """
-    fetch nicknames by usernames from db / cache
-    return: {username0: nickname0, username1: nickname1...}
-    """
-    if not usernames:
-        return {}
-    cache_timeout = 60*60*24
-    key_format = 'user:nickname:%s'
-    users_dict, miss_users = {}, []
-
-    for username in usernames:
-        nickname = cache.get(key_format % username)
-        if nickname is None:
-            miss_users.append(username)
-        else:
-            users_dict[username] = nickname
-            cache.set(key_format % username, nickname, timeout=cache_timeout)
-
-    if not miss_users:
-        return users_dict
-
-    # miss_users is not empty
-    sql = "SELECT user, nickname FROM profile_profile WHERE user in :users"
-    try:
-        for username, nickname in db_session.execute(sql, {'users': usernames}).fetchall():
-            users_dict[username] = nickname
-            cache.set(key_format % username, nickname, timeout=cache_timeout)
-    except Exception as e:
-        logger.error('check nicknames error: %s', e)
-
-    return users_dict
+    for item in filters:
+        if item.get('filter_predicate') == 'include_me':
+            item['filter_term'].append('')
+        elif item.get('filter_predicate') == 'is_current_user_ID':
+            item['filter_term'] = -1
+    filter_conditions['filters'] = filters
+    filter_conditions['filter_conjunction'] = filter_conjunction
+    sql = NearDeadlineSqlGenerator(table['name'], table['columns'], filter_conditions=filter_conditions).to_sql(date_column_name, alarm_days)
+    return dtable_db_api.query_and_metadata(sql, convert=False)
 
 
 def _get_geolocation_infos(cell_value_dict):
@@ -197,7 +208,7 @@ def _get_geolocation_infos(cell_value_dict):
 
     return info_list and " ".join(info_list) or ''
 
-def _fill_msg_blanks(msg, column_blanks, col_name_dict, row, db_session, dtable_metadata):
+def fill_msg_blanks_with_converted_row(msg, column_blanks, col_name_dict, converted_row, db_session, dtable_metadata):
     for blank in column_blanks:
         if col_name_dict[blank]['type'] in [
             ColumnTypes.TEXT,
@@ -212,14 +223,14 @@ def _fill_msg_blanks(msg, column_blanks, col_name_dict, row, db_session, dtable_
             ColumnTypes.CTIME,
             ColumnTypes.MTIME
         ]:
-            value = row.get(blank, '')
+            value = converted_row.get(blank, '')
             msg = msg.replace('{' + blank + '}', str(value) if value else '')  # maybe value is None and str(None) is 'None'
 
         elif col_name_dict[blank]['type'] in [
             ColumnTypes.IMAGE,
             ColumnTypes.MULTIPLE_SELECT
         ]:
-            value = row.get(blank, [])
+            value = converted_row.get(blank, [])
             if not value:
                 msg = msg.replace('{' + blank + '}', '[]')  # maybe value is None
             elif value and isinstance(value, list) and isinstance(value[0], str):
@@ -228,15 +239,15 @@ def _fill_msg_blanks(msg, column_blanks, col_name_dict, row, db_session, dtable_
                 logger.warning('column %s value format error', blank)
 
         elif col_name_dict[blank]['type'] in [ColumnTypes.LINK]:
-            value = row.get(blank, [])
+            value = converted_row.get(blank, [])
             msg = msg.replace('{' + blank + '}', ('[' + ', '.join([f['display_value'] for f in value]) + ']') if value else '[]')
 
         elif col_name_dict[blank]['type'] in [ColumnTypes.FILE]:
-            value = row.get(blank, [])
+            value = converted_row.get(blank, [])
             msg = msg.replace('{' + blank + '}', ('[' + ', '.join([f['name'] for f in value]) + ']') if value else '[]')
 
         elif col_name_dict[blank]['type'] in [ColumnTypes.COLLABORATOR]:
-            users = row.get(blank, [])
+            users = converted_row.get(blank, [])
             if users is None:
                 names = []
             else:
@@ -245,7 +256,7 @@ def _fill_msg_blanks(msg, column_blanks, col_name_dict, row, db_session, dtable_
             msg = msg.replace('{' + blank + '}', '[' + ', '.join(names) + ']')
 
         elif col_name_dict[blank]['type'] in [ColumnTypes.CREATOR, ColumnTypes.LAST_MODIFIER]:
-            value = row.get(blank, '')
+            value = converted_row.get(blank, '')
             if value is None:
                 name = ''
             else:
@@ -258,7 +269,7 @@ def _fill_msg_blanks(msg, column_blanks, col_name_dict, row, db_session, dtable_
             # we need to fill blanks with nicknames,
             # else just transfer value to str to fill blanks.
             # Judge whether value is like collaborator or not is base on metadata of dtable
-            value = row.get(blank)
+            value = converted_row.get(blank)
 
             # If result of formula is a string or None, just replace directly
             if isinstance(value, str) or value is None:
@@ -306,7 +317,7 @@ def _fill_msg_blanks(msg, column_blanks, col_name_dict, row, db_session, dtable_
                     msg = msg.replace('{' + blank + '}', str(value))
 
         elif col_name_dict[blank]['type'] in [ColumnTypes.GEOLOCATION]:
-            value = row.get(blank, '')
+            value = converted_row.get(blank, '')
             value = value and _get_geolocation_infos(value) or ''
             msg = msg.replace('{' + blank + '}', str(value) if value else '')
 
@@ -314,7 +325,29 @@ def _fill_msg_blanks(msg, column_blanks, col_name_dict, row, db_session, dtable_
     return msg
 
 
-def _get_column_blanks(blanks, columns):
+def fill_msg_blanks_with_sql_row(msg, column_blanks, col_name_dict, row, db_session):
+    for blank in column_blanks:
+        value = row.get(col_name_dict[blank]['key'])
+        column_type = col_name_dict[blank]['type']
+        formatter_class = formatter_map.get(column_type)
+        if not formatter_class:
+            continue
+        params = create_formatter_params(column_type, value=value, db_session=db_session)
+        if value is None:
+            message = formatter_class(col_name_dict[blank]).format_empty_message()
+            msg = msg.replace('{' + blank + '}', str(message))
+            continue
+        try:
+            message = formatter_class(col_name_dict[blank]).format_message(**params)
+            msg = msg.replace('{' + blank + '}', str(message))
+        except Exception as e:
+            logger.exception(e)
+            msg = msg.replace('{' + blank + '}', '')
+
+    return msg
+
+
+def get_column_blanks(blanks, columns):
     col_name_dict = {col['name']: col for col in columns}
 
     column_blanks = []
@@ -327,16 +360,24 @@ def _get_column_blanks(blanks, columns):
     return column_blanks, col_name_dict
 
 
-def gen_notification_msg_with_row(msg, row, column_blanks, col_name_dict, db_session, dtable_metadata=None):
+def gen_noti_msg_with_converted_row(msg, row, column_blanks, col_name_dict, db_session, dtable_metadata=None):
     if not msg:
         return msg
     if not column_blanks:
         return msg
 
-    return _fill_msg_blanks(msg, column_blanks, col_name_dict, row, db_session, dtable_metadata=dtable_metadata)
+    return fill_msg_blanks_with_converted_row(msg, column_blanks, col_name_dict, row, db_session, dtable_metadata=dtable_metadata)
 
 
-def _get_column_by_key(dtable_metadata, table_id, column_key):
+def gen_noti_msg_with_sql_row(msg, row, column_blanks, col_name_dict, db_session):
+    if not msg:
+        return msg
+    if not column_blanks:
+        return msg
+    return fill_msg_blanks_with_sql_row(msg, column_blanks, col_name_dict, row, db_session)
+
+
+def get_column_by_key(dtable_metadata, table_id, column_key):
     table = None
     for t in dtable_metadata.get('tables', []):
         if t.get('_id') == table_id:
@@ -405,7 +446,7 @@ def trigger_notification_rule(rule, message_table_id, row, converted_row, dtable
     blanks, column_blanks, col_name_dict = set(re.findall(r'\{([^{]*?)\}', msg)), None, None
     if blanks:
         columns = target_table['columns']
-        column_blanks, col_name_dict = _get_column_blanks(blanks, columns)
+        column_blanks, col_name_dict = get_column_blanks(blanks, columns)
 
     if op_type == 'modify_row' and trigger['condition'] == CONDITION_ROWS_MODIFIED:
         if not is_trigger_time_satisfy(last_trigger_time):
@@ -417,12 +458,12 @@ def trigger_notification_rule(rule, message_table_id, row, converted_row, dtable
             'condition': CONDITION_ROWS_MODIFIED,
             'rule_id': rule.id,
             'rule_name': rule_name,
-            'msg': gen_notification_msg_with_row(msg, converted_row, column_blanks, col_name_dict, db_session, dtable_metadata=dtable_metadata),
+            'msg': gen_noti_msg_with_converted_row(msg, converted_row, column_blanks, col_name_dict, db_session, dtable_metadata=dtable_metadata),
             'row_id_list': [row['_id']],
         }
 
         if users_column_key:
-            user_column = _get_column_by_key(dtable_metadata, table_id, users_column_key)
+            user_column = get_column_by_key(dtable_metadata, table_id, users_column_key)
             if user_column:
                 users_column_name = user_column.get('name')
                 users_from_column = converted_row.get(users_column_name, [])
@@ -452,11 +493,11 @@ def trigger_notification_rule(rule, message_table_id, row, converted_row, dtable
             'condition': CONDITION_FILTERS_SATISFY,
             'rule_id': rule.id,
             'rule_name': rule_name,
-            'msg': gen_notification_msg_with_row(msg, converted_row, column_blanks, col_name_dict, db_session, dtable_metadata=dtable_metadata),
+            'msg': gen_noti_msg_with_converted_row(msg, converted_row, column_blanks, col_name_dict, db_session, dtable_metadata=dtable_metadata),
             'row_id_list': [row['_id']],
         }
         if users_column_key:
-            user_column = _get_column_by_key(dtable_metadata, table_id, users_column_key)
+            user_column = get_column_by_key(dtable_metadata, table_id, users_column_key)
             if user_column:
                 users_column_name = user_column.get('name')
                 users_from_column = converted_row.get(users_column_name, [])
@@ -504,6 +545,7 @@ def trigger_near_deadline_notification_rule(rule, db_session):
 
     dtable_server_api = DTableServerAPI('notification-rule', dtable_uuid, get_inner_dtable_server_url())
     dtable_web_api = DTableWebAPI(DTABLE_WEB_SERVICE_URL)
+    dtable_db_api = DTableDBAPI('dtable-events', dtable_uuid, INNER_DTABLE_DB_URL)
     dtable_metadata = dtable_server_api.get_metadata()
     target_table, target_view = None, None
     for table in dtable_metadata['tables']:
@@ -552,8 +594,9 @@ def trigger_near_deadline_notification_rule(rule, db_session):
 
     dtable_server_access_token = get_dtable_server_token(dtable_uuid)
     try:
-        rows_near_deadline = list_rows_near_deadline(dtable_uuid, table_id, view_id, date_column_name, alarm_days, dtable_server_access_token, rule_id, db_session)
+        rows_near_deadline, sql_metadata = list_rows_near_deadline_with_dtable_db(dtable_metadata, table_id, view_id, date_column_name, alarm_days, dtable_db_api)
     except Exception as e:
+        logger.exception(e)
         logger.error('dtable: %s list rows_near_deadline failed. error: %s', dtable_uuid, e)
         return
 
@@ -562,16 +605,15 @@ def trigger_near_deadline_notification_rule(rule, db_session):
 
     blanks, column_blanks, col_name_dict = set(re.findall(r'\{([^{]*?)\}', msg)), None, None
     if blanks:
-        columns = target_table['columns']
-        column_blanks, col_name_dict = _get_column_blanks(blanks, columns)
+        column_blanks, col_name_dict = get_column_blanks(blanks, sql_metadata)
 
     for row in rows_near_deadline[:25]:
         row_id = row['_id']
 
         if users_column_key:
-            user_column = _get_column_by_key(dtable_metadata, table_id, users_column_key)
+            user_column = get_column_by_key(dtable_metadata, table_id, users_column_key)
             if user_column:
-                temp_users_from_cell = row.get(user_column['name'], [])
+                temp_users_from_cell = row.get(users_column_key, [])
                 if isinstance(temp_users_from_cell, list):
                     users_from_cell = temp_users_from_cell
                 elif isinstance(temp_users_from_cell, str):
@@ -590,7 +632,7 @@ def trigger_near_deadline_notification_rule(rule, db_session):
             'condition': CONDITION_NEAR_DEADLINE,
             'rule_id': rule.id,
             'rule_name': rule_name,
-            'msg': gen_notification_msg_with_row(msg, row, column_blanks, col_name_dict, db_session, dtable_metadata=dtable_metadata),
+            'msg': gen_noti_msg_with_sql_row(msg, row, column_blanks, col_name_dict, db_session),
             'row_id_list': [row_id],
         }
 
