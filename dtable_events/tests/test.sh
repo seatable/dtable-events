9dfe14aab507adbf1a55cc4c285345e1c31edb91
AlexCXC <1223408988@qq.com>
2023-07-19 13:47:32 +0800

    update test.sh add a fake dtable-web

--- dtable_events/tests/test.sh
+++ dtable_events/tests/test.sh
@@ -26,25 +26,36 @@ fi
 set -x
 EVENTS_TESTDIR=$(python -c "import os; print(os.path.dirname(os.path.realpath('$0')))")
 EVENTS_SRCDIR=$(dirname $(dirname "${EVENTS_TESTDIR}"))
+HOME_DIR=$(dirname "${EVENTS_SRCDIR}")
 
 export SEAHUB_LOG_DIR='/tmp/logs'
 export PYTHONPATH="/usr/local/lib/python3.8/site-packages:/usr/local/lib/python3.8/dist-packages:/usr/lib/python3.8/site-packages:/usr/lib/python3.8/dist-packages:${PYTHONPATH}"
 cd "$EVENTS_SRCDIR"
 set +x
 
+# Not need to init plugins repo
 # # init plugins repo
 # repo_id=$(python -c "from seaserv import seafile_api; repo_id = seafile_api.create_repo('plugins repo', 'plugins repo', 'dtable@seafile'); print(repo_id)")
 # sudo echo -e "\nPLUGINS_REPO_ID='"${repo_id}"'" >>./seahub/settings.py
 
 function init() {
-    ###############################
-    # create two new users: an admin, and a normal user
-    ###############################
-    # create normal user
-    $PYTHON -c "import os; from seaserv import ccnet_api; ccnet_api.add_emailuser('${SEAHUB_TEST_USERNAME}', '${SEAHUB_TEST_PASSWORD}', 0, 1);"
-    # create admin
-    $PYTHON -c "import os; from seaserv import ccnet_api; ccnet_api.add_emailuser('${SEAHUB_TEST_ADMIN_USERNAME}', '${SEAHUB_TEST_ADMIN_PASSWORD}', 1, 1);"
+    # Not need to create user now
+    # ###############################
+    # # create two new users: an admin, and a normal user
+    # ###############################
+    # # create normal user
+    # $PYTHON -c "import os; from seaserv import ccnet_api; ccnet_api.add_emailuser('${SEAHUB_TEST_USERNAME}', '${SEAHUB_TEST_PASSWORD}', 0, 1);"
+    # # create admin
+    # $PYTHON -c "import os; from seaserv import ccnet_api; ccnet_api.add_emailuser('${SEAHUB_TEST_ADMIN_USERNAME}', '${SEAHUB_TEST_ADMIN_PASSWORD}', 1, 1);"
 
+    if [ -d $HOME_DIR/dtable_web ]; then
+        echo 'dtable-web exists'
+    # a fake dtable-web
+    else
+        mkdir -p $HOME_DIR/dtable_web/seahub
+        touch $HOME_DIR/dtable_web/seahub/settings.py
+    fi
+    export DTABLE_WEB_DIR=$HOME_DIR/dtable_web
 }
 
 function run_tests() {
