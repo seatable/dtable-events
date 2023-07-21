011b2698e62c9d3cbe7259d5aedc7b1f7d7b9b69
r350178982 <32759763+r350178982@users.noreply.github.com>
2023-07-21 22:48:40 +0800

    tests one -try

--- dtable_events/tests/sql/column_reference.py
+++ dtable_events/tests/sql/column_reference.py
@@ -15,7 +15,7 @@ TEST_COLUMNS = [
     {'key': '51r0', 'type': 'number', 'name': 'Num', 'editable': True, 'width': 200, 'resizable': True, 'draggable': True, 'data': {'format': 'number', 'precision': 2, 'enable_precision': False, 'enable_fill_default_value': False, 'decimal': 'dot', 'thousands': 'no'}, 'permission_type': '', 'permitted_users': []}
 ]
 
-LINK_COLUMNS = {"key":"G5J9","type":"link","name":"Link","editable":True,"width":200,"resizable":True,"draggable":True,"data":{"display_column_key":"0000","table_id":"0000","other_table_id":"kzvB","is_internal_link":True,"is_multiple":True,"is_row_from_view":False,"other_view_id":"","link_id":"55Sl","array_type":"text","array_data":None,"result_type":"array"},"permission_type":"","permitted_users":[],"edit_metadata_permission_type":"","edit_metadata_permitted_users":[],"description":None}
+LINK_COLUMN = {"key":"G5J9","type":"link","name":"Link","editable":True,"width":200,"resizable":True,"draggable":True,"data":{"display_column_key":"0000","table_id":"0000","other_table_id":"kzvB","is_internal_link":True,"is_multiple":True,"is_row_from_view":False,"other_view_id":"","link_id":"55Sl","array_type":"text","array_data":None,"result_type":"array"},"permission_type":"","permitted_users":[],"edit_metadata_permission_type":"","edit_metadata_permitted_users":[],"description":None}
 
 TABLES = [{
 		"_id": "0000",
--- dtable_events/tests/sql/sql_test.py
+++ dtable_events/tests/sql/sql_test.py
@@ -4,9 +4,9 @@ import sys
 sys.path.append(sys.path.append(os.path.join(os.path.dirname(__file__), '../')))
 d = os.path.dirname
 sys.path.append(sys.path.append(d(d(d(d(__file__))))))
-from sql.column_reference import TEST_COLUMNS
+from sql.column_reference import TEST_COLUMNS, TABLES, LINK_COLUMN
 from sql.test_reference import TEST_CONDITIONS
-from dtable_events import filter2sql
+from dtable_events import filter2sql, linkRecords2sql
 
 class SqlTest(unittest.TestCase):
 
@@ -22,12 +22,19 @@ class SqlTest(unittest.TestCase):
         return sql
 
     def test_equal(self):
+        tables = TABLES
+        current_table = TABLES[0]
+        link_column = LINK_COLUMN
+        sql = linkRecords2sql(current_table, link_column, ["ssssss", "eeeeeeee"], tables)
+        print(sql, 'e123123121212')
         for conditions in TEST_CONDITIONS:
             filter_conditions = conditions.get('filter_conditions')
             expected_sql = conditions.get('expected_sql')
             by_group = conditions.get('by_group')
             sql = self._toSql(filter_conditions, by_group=by_group)
             self.assertEqual(sql, expected_sql)
+        
+
 
 
 if __name__ == '__main__':
