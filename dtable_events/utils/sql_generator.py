43e464941da65d32b5596c2a0488e32a25caacbc
AlexCXC <1223408988@qq.com>
2023-08-18 16:09:21 +0800

    opt sql-unit-test for column not found ValueError

--- dtable_events/utils/sql_generator.py
+++ dtable_events/utils/sql_generator.py
@@ -1639,7 +1639,7 @@ class BaseSQLGenerator(object):
                 operator_cls = _get_operator_by_type(column_type)
                 if not operator_cls:
                     raise ValueError('filter: %s not support to sql', filter_item)
-                operator = (column, filter_item)
+                operator = operator_cls(column, filter_item)
                 sql_condition = _filter2sqlslice(operator)
                 if not sql_condition:
                     continue
@@ -1675,6 +1675,8 @@ class BaseSQLGenerator(object):
             column = column_key and self._get_column_by_key(column_key)
             if not column:
                 column = column_name and self._get_column_by_name(column_name)
+            if not column:
+                raise ValueError('Column not found column_key: %s column_name: %s', column_key, column_name)
             column_type = column.get('type')
             operator_cls = _get_operator_by_type(column_type)
             if not operator_cls:
