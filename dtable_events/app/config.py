bf9028a3688503096ae103fbc88d1d25de71e34f
AlexCXC <1223408988@qq.com>
2023-11-10 17:09:10 +0800

    fix unittest

--- dtable_events/app/config.py
+++ dtable_events/app/config.py
@@ -49,7 +49,7 @@ try:
     LICENSE_PATH = getattr(seahub_settings, 'LICENSE_PATH', '/shared/seatable-license.txt')
     IS_PRO_VERSION = getattr(seahub_settings, 'IS_PRO_VERSION', False)
     NEW_DTABLE_IN_STORAGE_SERVER = getattr(seahub_settings, 'NEW_DTABLE_IN_STORAGE_SERVER', False)
-    ENABLE_SYSTEM_BASES = getattr(seahub_settings, 'ENABLE_SYSTEM_BASES', True)
+    ENABLE_SYSTEM_BASES = getattr(seahub_settings, 'ENABLE_SYSTEM_BASES', False)
     SYSTEM_BASES_OWNER = getattr(seahub_settings, 'SYSTEM_BASES_OWNER', 'system bases')
 except Exception as e:
     logger.critical("Can not import dtable_web settings: %s." % e)
