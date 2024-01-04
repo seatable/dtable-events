773d7d367348673cc1c44c52d14cbafac0e45893
AlexCXC <1223408988@qq.com>
2024-01-04 16:24:04 +0800

    support transfer department-single-select cell value

--- dtable_events/common_dataset/common_dataset_sync_utils.py
+++ dtable_events/common_dataset/common_dataset_sync_utils.py
@@ -364,6 +364,13 @@ def get_converted_cell_value(dtable_db_cell_value, transfered_column, col):
     ]:
         return deepcopy(dtable_db_cell_value)
 
+    elif col_type == ColumnTypes.DEPARTMENT_SINGLE_SELECT:
+        try:
+            dtable_db_cell_value = int(str(dtable_db_cell_value))  # department-single-select need a int
+        except:
+            return
+        return dtable_db_cell_value
+
     elif col_type == ColumnTypes.SINGLE_SELECT:
         if not isinstance(dtable_db_cell_value, str):
             return
