004babcffb2ea7ad9d6e289ce6f9aa1188e32350
Michael An <2331806369@qq.com>
2024-04-10 12:03:14 +0800

    change apply_default default None

--- dtable_events/utils/dtable_server_api.py
+++ dtable_events/utils/dtable_server_api.py
@@ -231,7 +231,7 @@ class DTableServerAPI(object):
         response = requests.post(url, json=json_data, headers=self.headers, timeout=self.timeout)
         return parse_response(response)
 
-    def append_row(self, table_name, row_data, apply_default=False):
+    def append_row(self, table_name, row_data, apply_default=None):
         logger.debug('append row table_name: %s row_data: %s', table_name, row_data)
         url = self.dtable_server_url + '/api/v1/dtables/' + self.dtable_uuid + '/rows/?from=dtable_events'
         json_data = {
