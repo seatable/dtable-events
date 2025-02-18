a1488e0bf59e5f02f9e032d487830974017e0b44
AlexCXC <1223408988@qq.com>
2025-02-18 18:23:47 +0800

    add ai-stats

---
+++ dtable_events/tasks/ai_stats_worker.py
@@ -0,0 +1,193 @@
+import json
+import logging
+import time
+from collections import defaultdict
+from copy import deepcopy
+from datetime import datetime
+from threading import Thread, Lock
+
+from sqlalchemy import text
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+from dtable_events.app.config import AI_PRICES, BAIDU_OCR_TOKENS
+from dtable_events.app.event_redis import RedisClient
+from dtable_events.db import init_db_session_class
+from dtable_events.utils import get_opt_from_conf_or_env, parse_bool
+
+logger = logging.getLogger(__name__)
+
+
+class AIStatsWorker:
+
+    def __init__(self, config):
+        self._db_session_class = init_db_session_class(config)
+        self._redis_client = RedisClient(config)
+        self.stats_lock = Lock()
+        self.org_stats = defaultdict(lambda: defaultdict(lambda: {'input_tokens': 0, 'output_tokens': 0}))
+        self.user_stats = defaultdict(lambda: defaultdict(lambda: {'input_tokens': 0, 'output_tokens': 0}))
+        self._parse_config(config)
+        self.channel = 'log_ai_model_usage'
+
+    def _parse_config(self, config):
+        """parse send email related options from config file
+        """
+        section_name = 'AI STATS'
+        key_enabled = 'enabled'
+
+        # enabled
+        enabled = get_opt_from_conf_or_env(config, section_name, key_enabled, default=False)
+        enabled = parse_bool(enabled)
+        self._enabled = enabled
+
+    def save_to_memory(self, usage_info):
+        if not usage_info.get('model'):
+            return
+
+        model = usage_info['model']
+        usage = usage_info.get('usage')
+
+        if 'prompt_tokens' in usage:
+            usage['input_tokens'] = usage['prompt_tokens']
+        if 'completion_tokens' in usage:
+            usage['output_tokens'] = usage['completion_tokens']
+
+        if not isinstance(usage.get('input_tokens'), int):
+            usage['input_tokens'] = 0
+        if not isinstance(usage.get('output_tokens'), int):
+            usage['output_tokens'] = 0
+
+        if model in BAIDU_OCR_TOKENS:
+            usage['output_tokens'] = BAIDU_OCR_TOKENS[model]
+
+        org_id = usage_info.get('org_id')
+        username = usage_info.get('username')
+        if org_id and org_id != -1:
+            self.org_stats[org_id][model]['input_tokens'] += usage.get('input_tokens') or 0
+            self.org_stats[org_id][model]['output_tokens'] += usage.get('output_tokens') or 0
+        elif username:
+            self.user_stats[username][model]['input_tokens'] += usage.get('input_tokens') or 0
+            self.user_stats[username][model]['output_tokens'] += usage.get('output_tokens') or 0
+
+    def receive(self):
+        logger.info('Starting receiving ai calls...')
+        subscriber = self._redis_client.get_subscriber(self.channel)
+
+        while True:
+            try:
+                message = subscriber.get_message()
+                if message is not None:
+                    try:
+                        usage_info = json.loads(message['data'])
+                    except:
+                        logger.warning('log_ai_model_usage message invalid')
+                    logger.debug('usage_info %s', usage_info)
+                    with self.stats_lock:
+                        self.save_to_memory(usage_info)
+                else:
+                    time.sleep(0.5)
+            except Exception as e:
+                logger.error('Failed get message from redis: %s' % e)
+                subscriber = self._redis_client.get_subscriber(self.channel)
+
+    def stats_worker(self):
+        with self.stats_lock:
+            org_stats = deepcopy(self.org_stats)
+            user_stats = deepcopy(self.user_stats)
+            self.org_stats = defaultdict(lambda: defaultdict(lambda: {'input_tokens': 0, 'output_tokens': 0}))
+            self.user_stats = defaultdict(lambda: defaultdict(lambda: {'input_tokens': 0, 'output_tokens': 0}))
+
+        logger.info('There are %s org stats', len(org_stats))
+        logger.info('There are %s user stats', len(user_stats))
+
+        data = []
+        month = datetime.today().replace(day=1).date()
+        sql = '''
+        INSERT INTO `ai_stats`(`org_id`, `owner_id`, `month`, `model`, `input_tokens`, `output_tokens`, `cost`, `created_at`, `updated_at`) 
+        VALUES (:org_id, :owner_id, :month, :model, :input_tokens, :output_tokens, :cost, :created_at, :updated_at)
+        ON DUPLICATE KEY UPDATE `input_tokens`=`input_tokens`+VALUES(`input_tokens`),
+                                `output_tokens`=`output_tokens`+VALUES(`output_tokens`),
+                                `cost`=`cost`+VALUES(`cost`),
+                                `updated_at`=VALUES(`updated_at`)
+        '''
+        for org_id, models_dict in org_stats.items():
+            for model, usage in models_dict.items():
+                input_tokens = usage.get('input_tokens') or 0
+                output_tokens = usage.get('output_tokens') or 0
+
+                if model not in AI_PRICES:
+                    logger.info('org %s price of model %s not defined', org_id, model)
+                    continue
+                input_tokens_price = AI_PRICES[model].get('input_tokens_1k') or 0
+                output_tokens_price = AI_PRICES[model].get('output_tokens_1k') or 0
+                input_cost = input_tokens_price * (input_tokens / 1000)
+                output_cost = output_tokens_price * (output_tokens / 1000)
+                logger.info('org %s model %s, input_tokens %s cost %s, output_tokens %s cost %s', org_id, model, input_tokens, input_cost, output_tokens, output_cost)
+
+                params = {
+                    'org_id': org_id,
+                    'owner_id': None,
+                    'month': month,
+                    'model': model,
+                    'input_tokens': input_tokens,
+                    'output_tokens': output_tokens,
+                    'cost': input_cost + output_cost,
+                    'created_at': datetime.now(),
+                    'updated_at': datetime.now()
+                }
+                data.append(params)
+
+        for username, models_dict in user_stats.items():
+            for model, usage in models_dict.items():
+                input_tokens = usage.get('input_tokens') or 0
+                output_tokens = usage.get('output_tokens') or 0
+
+                if model not in AI_PRICES:
+                    logger.info('user %s price of model %s not defined', org_id, model)
+                    continue
+                input_tokens_price = AI_PRICES[model].get('input_tokens_1k') or 0
+                output_tokens_price = AI_PRICES[model].get('output_tokens_1k') or 0
+                input_cost = input_tokens_price * (input_tokens / 1000)
+                output_cost = output_tokens_price * (output_tokens / 1000)
+                logger.info('user %s model %s, input_tokens %s cost %s, output_tokens %s cost %s', org_id, model, input_tokens, input_cost, output_tokens, output_cost)
+
+                params = {
+                    'org_id': None,
+                    'owner_id': username,
+                    'month': month,
+                    'model': model,
+                    'input_tokens': input_tokens,
+                    'output_tokens': output_tokens_price,
+                    'cost': input_cost + output_cost,
+                    'created_at': datetime.now(),
+                    'updated_at': datetime.now()
+                }
+                data.append(params)
+
+        if not data:
+            return
+
+        session = self._db_session_class()
+        try:
+            session.execute(text(sql), data)
+            session.commit()
+        except Exception as e:
+            logger.exception(e)
+        finally:
+            session.close()
+
+    def stats(self):
+        sched = BlockingScheduler()
+        # fire at 0,30 in every hour
+        @sched.scheduled_job('cron', day_of_week='*', hour='*', minute='0,30', misfire_grace_time=600)
+        def timed_job():
+            logging.info('Starts to stats ai calls in memory...')
+            self.stats_worker()
+
+        sched.start()
+
+    def start(self):
+        if not self._enabled:
+            logger.warning('Can not stats AI: it is not enabled!')
+            return
+        Thread(target=self.receive, daemon=True).start()
+        Thread(target=self.stats, daemon=True).start()
