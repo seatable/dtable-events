4d1071a19515af10f42b87f2f8af70b0f799fedc
Alex Happy <1223408988@qq.com>
2025-02-18 10:09:30 +0800

    update org exceed-api-quota after stats api calls (#710)
    * call dtable-web update-exceed-api-quota after each stats and and delete exceed at 1st every month
    
    * update some logs
    
    * fix sql-test

--- dtable_events/api_calls/api_calls_counter.py
+++ dtable_events/api_calls/api_calls_counter.py
@@ -10,9 +10,11 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from dateutil import parser, relativedelta
 from sqlalchemy import text
 
+from dtable_events.app.config import DTABLE_WEB_SERVICE_URL
 from dtable_events.app.event_redis import RedisClient
 from dtable_events.db import init_db_session_class
 from dtable_events.utils import uuid_str_to_32_chars
+from dtable_events.utils.dtable_web_api import DTableWebAPI
 
 logger = logging.getLogger(__name__)
 
@@ -96,6 +98,15 @@ class APICallsCounter:
             db_session.execute(text(sql))
             db_session.commit()
 
+        # update exceed status
+        org_ids = list(org_counts_dict.keys())
+        if org_ids:
+            dtable_web_api = DTableWebAPI(DTABLE_WEB_SERVICE_URL)
+            try:
+                dtable_web_api.internal_update_exceed_api_quota(month, org_ids)
+            except Exception as e:
+                logger.exception('update exceed api quota error: %s', e)
+
     def count_api_calls(self, info, db_session):
         component = info.get('component')
         try:
@@ -147,6 +158,31 @@ class APICallsCounter:
 
         sched.start()
 
+    def reset(self):
+        logger.info('Starting schedule reset api calls exceed status...')
+        sched = BlockingScheduler()
+        # fire at 0 o'clock in every day of week
+        @sched.scheduled_job('cron', month='*', day='1', day_of_week='*', hour='0', misfire_grace_time=600)
+        def timed_job():
+            session = self._db_session_class()
+            sql = "DELETE FROM `exceed_api_quota_teams`"
+            logger.info('Start to reset exceed_api_quota_teams')
+            try:
+                session.execute(text(sql))
+                session.commit()
+            except Exception as e:
+                logger.exception('reset api calls exceed status error: %s', e)
+            finally:
+                session.close()
+
+            try:
+                self._redis_client.connection.publish('exceed_api_quota', json.dumps({'changed': True}))
+            except Exception as e:
+                logger.exception('publish exceed_api_quota error: %s', e)
+
+        sched.start()
+
     def start(self):
         Thread(target=self.count, daemon=True).start()
         Thread(target=self.clean, daemon=True).start()
+        Thread(target=self.reset, daemon=True).start()
