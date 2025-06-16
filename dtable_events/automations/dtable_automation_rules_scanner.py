ac667e7ff14563b81c242edc61c0cfc5d4171eb4
AlexCXC <1223408988@qq.com>
2025-06-16 11:04:32 +0800

    remove useless code

--- dtable_events/automations/dtable_automation_rules_scanner.py
+++ dtable_events/automations/dtable_automation_rules_scanner.py
@@ -139,7 +139,7 @@ class DTableAutomationRulesScannerTimer(Thread):
     def run(self):
         sched = BlockingScheduler()
         # fire at every hour in every day of week
-        @sched.scheduled_job('cron', day_of_week='*', hour='*', minute='28', misfire_grace_time=600)
+        @sched.scheduled_job('cron', day_of_week='*', hour='*', misfire_grace_time=600)
         def timed_job():
             logging.info('Starts to scan automation rules...')
 
