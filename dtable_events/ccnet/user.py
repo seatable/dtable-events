789fcfedbe07196dd71dc01177499572f9b99875
Alex Happy <1223408988@qq.com>
2025-11-12 11:51:10 +0800

    fix get user role in ccnet

--- dtable_events/ccnet/user.py
+++ dtable_events/ccnet/user.py
@@ -6,4 +6,6 @@ from dtable_events.app.config import CCNET_DB_NAME
 def get_user_role(db_session, username):
     sql = f"SELECT `role` FROM `{CCNET_DB_NAME}`.`UserRole` WHERE `email`=:email"
     row = db_session.execute(text(sql), {'email': username}).fetchone()
-    return getattr(row, 'role', )
+    if not row:
+        return 'default'
+    return getattr(row, 'role', 'default')
