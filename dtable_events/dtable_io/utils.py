19ba6d6b0fb4bf5d4f50675e71c991be34faef12
Alex Happy <1223408988@qq.com>
2026-09-11 15:10:54 +0800

    optimize excel update import: query only needed columns instead of select *

--- dtable_events/dtable_io/utils.py
+++ dtable_events/dtable_io/utils.py
@@ -1512,7 +1512,7 @@ def to_python_boolean(string):
     raise ValueError("Invalid boolean value: '%s'" % string)
 
 
-def get_rows_from_dtable_db(dtable_db_api, table_name, limit=50000):
+def get_rows_from_dtable_db(dtable_db_api, table_name, limit=50000, columns=None):
     from dtable_events.utils.dtable_db_api import convert_db_rows
     offset = 10000
     start = 0
@@ -1522,7 +1522,11 @@ def get_rows_from_dtable_db(dtable_db_api, table_name, limit=50000):
         if (start + offset) > limit:
             offset = limit - start
 
-        sql = f"SELECT * FROM `{table_name}` LIMIT {start}, {offset}"
+        if columns:
+            column_names_str = ', '.join('`%s`' % name for name in columns)
+            sql = f"SELECT {column_names_str} FROM `{table_name}` LIMIT {start}, {offset}"
+        else:
+            sql = f"SELECT * FROM `{table_name}` LIMIT {start}, {offset}"
 
         response_rows, metadata = dtable_db_api.query(sql, convert=False, server_only=True)
         response_rows = convert_db_rows(metadata, response_rows)
